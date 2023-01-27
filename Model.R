8c2c15ae7d9e5d2161ab276e2320e26d61adf9c2
Chloe Yip <chloetyip@yahoo.com>
2023-01-26 16:57:08 -0800

    new assessment function, better sampling function, life cycle. contains errors

--- Model.R
+++ Model.R
@@ -26,7 +26,7 @@
 # The focal agent will then determine the sex of all agents in their neighborhood.
 #
 # #Date#
-# At the start of the life cycle, a random agent will be paired with a random opposite-sex partner. Dating agents' number of dates will be incremented by one. 
+# At the start of the life cycle, a random agent will be paired with a random opposite-sex partner within their neighbrhood. Dating agents' number of dates will be incremented by one. 
 # 
 # #Select mates#
 # Next, each dating agent will decide whether to make a long-term commitment offer to their dating partner. For Rule 1, the probability of making an offer will be proportional to their date's attractiveness, adjusted by the number of dates the agent has gone on. For Rule 2, the probability of making an offer will be proportional to their date’s similarity, adjusted by the number of dates the agent has gone on. 
@@ -72,7 +72,7 @@ agentgenerate<-function(n,sex){
   physatt<-runif(n,0,10)
   
   #Assign agents a decision rule
-  rule<-sample(c("rule 1","rule 2"),n,replace=T,prob=1/2)
+  rule<-sample(c("rule 1","rule 2"),n,replace=T)
   
   #Create a vector to store the agents' date memories
   numdates<-0
@@ -97,30 +97,16 @@ agentgenerate<-function(n,sex){
 
 
 #Assessment Function#
-assess<-function(agents,popsize,radius){
+assess<-function(focal,target){
   
-  #Determine the distance between each agent and every other agent
-  distances<-as.matrix(dist(agents[,2:3]))
-  
-  #Generate a blank vector to store date decision
-  datedecision<-rep(NA,popsize)
-  
-  for (a in 1:popsize){
+  #Determine which agents are in an agent's neighborhood
+  date<-apply(focal,1,function(x)
+    dist(rbind(x[5:6],target[,5:6]))<radius
     
-    #Determine which agents are in agent a's neighborhood
-    neighbors<-distances[a,]<=radius
-    
-    #Determine which agents are of the opposite sex as agent a
-    oppositesex<-agents$sex[a]!=agents$sex
-    
-    #Have the agent decide whether they want to date
-    datedecision[a]<-oppositesex==T
-    
-  }
-  
-  #Output the agents' decisions
-  return(datedecision)
-  
+    )
+ 
+  return(date)
+   
 }
 
 
@@ -148,10 +134,117 @@ move<-function(agents){
 
 
 
+#Better Sampling#
+#A sample function that returns a single value
+bettersample<-function(x,size,replace=FALSE,prob=NULL){
+  
+  #If the sample vector has more than 1 element...
+  if(length(x)>1){
+    #Use sample just like normal
+    x<-sample(x,size,replace,prob)
+  }
+  
+  #Output the sample result
+  return(x)
+}
+
+
 
 #####Model Start#####
 
+#Generate agents
+females<-agentgenerate(popsize/2,"female")
+males<-agentgenerate(popsize/2,"male")
 
+#Create empty vectors to store paired agents
+pairedfemales<-c()
+pairedmales<-c()
+
+#Save the agents' starting positions
+femalestartdata<-females
+malestartdata<-males
+
+###Life Cycle###
+
+#Set the maximum number of steps the while loop can run
+maxsteps<-100
+
+#Set the current number of steps that have run
+steps<-0
+
+while(steps<maxsteps){
+  #Assess#
+  assess(males,females)
+  assess(females,males)
+  
+  
+  #Date#
+  
+  #Select a random single male and single female in the same neighborhood to date
+  fdate<-bettersample(females$ID[!(females$ID %in% pairedfemales)]&females$date==TRUE,1)
+  mdate<-bettersample(males$ID[!(males$ID %in% pairedmales)]&males$date==TRUE,1)
+  
+  #Increment the number of date agents have been on by 1
+  females$numdates[fdate]<-females$numdates[fdate]+1
+  males$numdates[mdate]<-males$numdates[mdate]+1
+  
+  
+  #Offer#
+  
+  #Determine how selective agents will be based on number of dates completed so far 
+  fchoosy<-((maxdates+1)-females$numdates[fdate])/maxdates
+  mchoosy<-((maxdates+1)-males$numdates[mdate])/maxdates
+  
+  #Prevent choosiness values less than 0
+  fchoosy<-ifelse(fchoosy>0,fchoosy,0)
+  mchoosy<-ifelse(mchoosy>0,mchoosy,0)
+  
+  #If the agent abides by Rule 1...
+  if(females$rule[fdate]=="rule 1"|males$rule[mdate]=="rule 1"){
+    
+    #Set the probability of a commitment offer based on date attractiveness and
+    #agent selectiveness
+    fp<-((males$physatt[mdate]^3)/1000)^fchoosy
+    mp<-((females$physatt[fdate]^3)/1000)^mchoosy
+    
+    #Have each agent probabilistically make an offer of commitment
+    foffer<-rbinom(1,1,fp)
+    moffer<-rbinom(1,1,mp)
+    
+  } else {
+    #Otherwise, if the agent abides by rule 2...
+    
+    #Set the probability of a commitment offer based on date similarity and
+    #agent selectiveness
+    fp<-(10-abs(males$physatt[mdate]-females$physatt[fdate])/10)^fchoosy
+    mp<-(10-abs(females$physatt[fdate]-males$physatt[mdate])/10)^mchoosy
+    
+    #Have each agent probabilistically make an offer of commitment
+    foffer<-rbinom(1,1,fp)
+    moffer<-rbinom(1,1,mp)
+    
+  }
+  
+  
+  #Pair#
+  
+  #If both agents make a mutual offer of commitment...
+  if(foffer & moffer){
+    
+    #...add their ID numbers to the paired agent vectors
+    pairedfemales<-c(pairedfemales,fdate)
+    pairedmales<-c(pairedmales,mdate)
+    
+    #Generate two new agents
+    
+  }
+
+}
+
+#Save the agents' ending positions
+femalesenddata<-females
+malesenddata<-males
+  
 
 
 
